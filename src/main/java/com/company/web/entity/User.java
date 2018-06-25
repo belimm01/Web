d31a91d13c83fb60eadb913d5c286de8a6075e0c
mkyoung <m.belimenko228@gmail.com>
2018-06-25 21:36:31 +0200

    Create Account and User entity

---
+++ src/main/java/com/company/web/entity/User.java
@@ -0,0 +1,34 @@
+package com.company.web.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String firstName;
+    private String secondName;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+}
