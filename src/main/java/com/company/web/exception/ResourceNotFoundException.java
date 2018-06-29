bcbe6b5a8b8fd4f2ec4fd2f9380b5954ee180a95
Ivona Vachova <ivona.vachova@seznam.cz>
2018-06-29 10:11:48 +0200

    Added some documentation

--- src/main/java/com/company/web/exception/ResourceNotFoundException.java
+++ src/main/java/com/company/web/exception/ResourceNotFoundException.java
@@ -7,26 +7,43 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 //with a given id is not found in the database.
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
+/**
+ * Exception when the resource is not found.
+ */
 public class ResourceNotFoundException extends RuntimeException {
     private String resourceName;
     private String fieldName;
     private Object fieldValue;
-
+/**
+ * Exception when the resourceName or fieldName of fieldValue disagree with database.
+ * @param resourceName 
+ * @param fieldName Name of a field in  database.
+ * @param fieldValue Value of searched field in database.
+ */
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
         super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
     }
-
+/**
+ * Getter for resourceName.
+ * @return got resourceName in form String.
+ */
     public String getResourceName() {
         return resourceName;
     }
-
+/**
+ * Getter for fieldName.
+ * @return got fieldName in form String.
+ */
     public String getFieldName() {
         return fieldName;
     }
-
+/**
+ * Getter for fieldValue.
+ * @return got fieldValue in form Object.
+ */
     public Object getFieldValue() {
         return fieldValue;
     }
